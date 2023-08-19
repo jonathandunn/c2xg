e7763b836a7d876b7b4338e0d20b984bf6ac3e57
AllieOsborne <130039371+AllieOsborne@users.noreply.github.com>
2023-08-19 14:15:07 +1200

    Dependencies quick-fix
    C2xG uses "clean-text", not "cleantext", though both are imported as the latter and have the share the function "clean()"

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
 	keywords = "grammar induction, syntax, cxg, unsupervised learning, natural language processing, computational linguistics, construction grammar, cognitive linguistics, usage-based grammar",
 	url = "http://www.c2xg.io",
 	packages = find_packages(exclude=["*.pyc", "__pycache__"]),
-	install_requires=["cleantext",
+	install_requires=["clean-text",
 			  "cytoolz",
 			  "gensim",
 			  "kmedoids",
