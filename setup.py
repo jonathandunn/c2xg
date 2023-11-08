9f5fa52c09aed739ce9118d57e7a8eba4d3f6b59
AllieOsborne <130039371+AllieOsborne@users.noreply.github.com>
2023-11-09 10:45:09 +1300

    Add version details
    Added version details. Just commented them out for now since they're not necessarily the ones to put in the main branch.

--- setup.py
+++ setup.py
@@ -16,16 +16,16 @@ setup(
 	keywords = "grammar induction, syntax, cxg, unsupervised learning, natural language processing, computational linguistics, construction grammar, cognitive linguistics, usage-based grammar",
 	url = "http://www.c2xg.io",
 	packages = find_packages(exclude=["*.pyc", "__pycache__"]),
-	install_requires=["clean-text",
-			  "cytoolz",
-			  "gensim",
-			  "kmedoids",
-			  "matplotlib",
-			  "numpy",
-			  "pandas",
-			  "scipy",
-			  "scikit-learn",
-			  "statsmodels"],
+	install_requires=["clean-text", #==0.6.0"
+			  "cytoolz", #==0.12.2"
+			  "gensim", #==4.3.0"
+			  "kmedoids", #==0.4.3"
+			  "matplotlib", #==3.7.2"
+			  "numpy", #==1.25.2"
+			  "pandas", #==2.0.3"
+			  "scipy", #==1.11.1"
+			  "scikit-learn", #==1.3.0"
+			  "statsmodels"], #==0.14.0"],
 	include_package_data=True,
 	long_description=read('README.md'),
 	long_description_content_type='text/markdown',
