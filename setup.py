4184d84c89ad9fb3d0bf8a1af97c6937b6da2a23
Jonathan Dunn <jonathan.dunn@canterbury.ac.nz>
2023-11-10 16:20:54 +1300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,24 +8,25 @@ def read(fname):
 
 setup(
 	name = "c2xg",
-	version = "1.0",
+	version = "2.0",
 	author = "Jonathan Dunn",
-	author_email = "jonathan.dunn@canterbury.ac.nz",
+	author_email = "jedunn@illinois.edu",
 	description = ("Construction Grammars for Natural Language Processing and Computational Linguistics"),
 	license = "LGPL 3.0",
 	keywords = "grammar induction, syntax, cxg, unsupervised learning, natural language processing, computational linguistics, construction grammar, cognitive linguistics, usage-based grammar",
 	url = "http://www.c2xg.io",
 	packages = find_packages(exclude=["*.pyc", "__pycache__"]),
-	install_requires=["clean-text", #==0.6.0"
-			  "cytoolz", #==0.12.2"
-			  "gensim", #==4.3.0"
-			  "kmedoids", #==0.4.3"
-			  "matplotlib", #==3.7.2"
-			  "numpy", #==1.25.2"
-			  "pandas", #==2.0.3"
-			  "scipy", #==1.11.1"
-			  "scikit-learn", #==1.3.0"
-			  "statsmodels"], #==0.14.0"],
+	install_requires=["clean-text >= 0.6.0",
+			  "cytoolz >= 0.12.2",
+			  "gensim >= 4.3.0",
+			  "kmedoids >= 0.4.3",
+			  "matplotlib >= 3.7.2",
+			  "numpy >= 1.25.2",
+			  "pandas >= 2.0.3",
+			  "scipy >= 1.11.1",
+			  "scikit-learn >= 1.3.0",
+			  "statsmodels >= 0.14.0",
+			 ],
 	include_package_data=True,
 	long_description=read('README.md'),
 	long_description_content_type='text/markdown',
